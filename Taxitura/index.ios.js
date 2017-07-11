621607c2024ca1e2b21ee2a0897ed3e72f02c79f
Cristian C. Zapata Torres (Cryzat) <cristianzapatat@gmail.com>
2017-07-11 15:11:39 -0500

    Eliminación de test, Se añade projecto Demo

---
+++ Taxitura/index.ios.js
@@ -0,0 +1,7 @@
+import {
+  AppRegistry
+} from 'react-native'
+
+import Taxitura from './app/index'
+
+AppRegistry.registerComponent('Taxitura', () => Taxitura)
--- test_socket_io/index.ios.js
+++
@@ -1,7 +0,0 @@
-import {
-  AppRegistry
-} from 'react-native'
-
-import Taxitura from './app/index'
-
-AppRegistry.registerComponent('test_socket_io', () => Taxitura)
